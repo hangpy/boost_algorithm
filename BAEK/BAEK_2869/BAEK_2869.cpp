8ea8ab33b9fe84f883ef9e9c0a6c62106027f590
hang <leehangbok2009@gmail.com>
2019-09-17 13:47:00 +0900

    Add baek 2869

---
+++ BAEK/BAEK_2869/BAEK_2869.cpp
@@ -0,0 +1,11 @@
+﻿// BAEK_2869.달팽이는 올라가고 싶다
+
+#include <iostream>
+using namespace std;
+int A, B, V;
+int main()
+{
+	cin >> A >> B >> V;
+	cout << (V - A) / (A - B) + 1 * ((V - A) % (A - B) != 0) + 1 << endl;
+	return 0;
+}
