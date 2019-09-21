1677504eedfaa645afa7b9151d6ca2657ae0abac
hangpy <leehangbok2009@gmail.com>
2019-09-21 13:12:46 +0900

    Add baek 1976, 9938 (not solved yet)

---
+++ BAEK/BAEK_1976/BAEK_1976.cpp
@@ -0,0 +1,57 @@
+﻿// BAEK_1976. 여행
+
+#include <iostream>
+#include <utility>
+
+using namespace std;
+
+int N, M, parent[201];
+
+int find(int p)
+{
+	if (p == parent[p]) return p;
+	else return parent[p] = find(parent[p]);
+}
+
+void merge(int l, int r)
+{
+	int lp = find(l);
+	int rp = find(r);
+
+	if (lp > rp) swap(lp, rp);
+	parent[rp] = lp;
+}
+
+
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(0);
+
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) parent[i] = i;
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= N; j++)
+		{
+			int con; cin >> con;
+			if (i < j) if(con) merge(i, j);
+		}
+	}
+	
+	int allConnected = true; int f; cin >> f;
+	int tmp = find(f);
+	for (int i = 1; i < M; i++)
+	{
+		int t; cin >> t;
+		if (tmp != find(t)) 
+		{
+			allConnected = false;
+			break;
+		}
+	}
+	if (allConnected) cout << "YES" << "\n";
+	else cout << "NO" << "\n";
+
+
+	return 0;
+}
---
+++ BAEK/BAEK_9938/BAEK_9938.cpp
@@ -0,0 +1,65 @@
+﻿// BAEK_9938. 방 청소
+
+#include <iostream>
+#include <cstring>
+#include <utility>
+#define endl "\n"
+
+using namespace std;
+
+int N, L, parent[300001], V[300001];
+
+int find(int p)
+{
+	if (parent[p] == p) return p;
+	else return parent[p] = find(parent[p]);
+}
+
+void merge(int a, int b)
+{
+	int ap = find(a), bp = find(b);
+	if (ap == bp) return;
+	if (ap > bp) swap(ap, bp);
+	parent[bp] = ap;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(0);
+	cin >> N >> L;
+	for (int i = 1; i <= L; i++) parent[i] = i;
+	for (int i = 1; i <= N; i++)
+	{
+		int a, b; cin >> a >> b; 
+		int tmp = 0;
+
+		if (!V[a]) V[a] =1, cout << "LADICA" << endl;
+		else if (!V[b]) V[b] = 1, cout << "LADICA" << endl;
+		else if((tmp = find(a)) == -1)
+		{
+			V[tmp] = 1; cout << "LADICA" << endl;
+		}
+		else if ((tmp = find(b)) == -1)
+		{
+			V[tmp] = 1; cout << "LADICA" << endl;
+		}
+		else cout << "SMECE" << endl;
+		merge(a, b);
+	}
+	return 0;
+}
+
+/*
+10 8
+1 2 
+1 2 
+1 3 
+3 4 
+1 3 
+5 8 
+7 6 
+6 7 
+7 5 
+8 7
+
+*/
